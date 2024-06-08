e3536dab0c3cc13794cdc9c6583e50fdeb6dd3c0
Garrett Smith <garrettarhsmith@gmail.com>
2024-06-08 10:12:57 -0400

    Some progress on work tiles

--- src/sections/Work/Work.tsx
+++ src/sections/Work/Work.tsx
@@ -6,19 +6,24 @@ const workItems = [
   {
     id: 0,
     title: "Express",
-    img: "https://www.express.com/content/dam/logos-fonts/logos/express/express-logo.svg",
+    img: "/express-home-page.jpg",
+    description: "I worked at express for 2 years working on the ecommerce website using React, GraphQL, and Jest/React Testing Library. I worked on some diverse, agile teams that included members of the Product, UX, QA, Backend, IOS, Android, and Web teams."
   },
   {
     id: 1,
-    title: "Tic-Tac-Toe"
+    title: "Tic-Tac-Toe",
+    img: "/tic-tac-toe.jpg",
+    description: "This is a project I'm working on using NextJS and Supabase. It's a realtime, online-multiplayer game."
   },
   {
     id: 2,
-    title: "This Portfolio"
+    title: "This Portfolio",
+    description: "I have gone through many iterations and redesigns for my portfolio. What I am currently set on is this, the one you're looking at. Very minimalist. It's a work in progress, but it's getting there. I am using NextJS.",
   },
   {
     id: 3,
-    title: "V School Projects"
+    title: "V School Projects",
+    description: "These projects were made during my time as a bootcamp student. Some of them may be a little rough or unfinished, but they showcase the use of APIs, MongoDB, ExpressJS, Mongoose, and React of course."
   },
 ]
 
@@ -29,13 +34,18 @@ export default function Work() {
       <div className={s.workItems}>
         {workItems.map(item => (
           <Tile key={item.id}>
-            <h3>{item.title}</h3>
             {item.img && <Image
               src={item.img}
               alt={item.title}
-              width={328}
-              height={54}
+              sizes="100vw"
+              style={{objectFit: "cover", objectPosition: "center top", maskImage: "linear-gradient(rgb(0, 0, 0), rgba(0, 0, 0, 0))", width: '100%', height: 'auto', marginBottom: "-5rem"}}
+              width={500}
+              height={300}
             />}
+            <div className={s.content}>
+              <h3 className={s.title}>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
           </Tile>
         ))}
       </div>
